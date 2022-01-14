b77871fd8443898dc19238bd67ea19405bc38ab7
Kenneth Johnson <kd123johnson@gmail.com>
2022-01-14 11:34:39 -0700

    Finished Map project

--- MobileMapperHackwich/ViewController.swift
+++ MobileMapperHackwich/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate
     
     var currentLocation: CLLocation!
     
+    var parks: [MKMapItem] = []
     
     let locationManager = CLLocationManager()
     
@@ -36,7 +37,36 @@ class ViewController: UIViewController, CLLocationManagerDelegate
         currentLocation = locations[0]
     }
     
+    @IBAction func whenZoomButtonPressed(_ sender: Any)
+    {
+        let coordinateSpan = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let center = currentLocation.coordinate
+        let region = MKCoordinateRegion(center: center, span: coordinateSpan)
+        mapViewA.setRegion(region, animated: true)
+    }
     
+    @IBAction func whenSearchButtonPressed(_ sender: Any)
+    {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = "Parks"
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        request.region = MKCoordinateRegion(center: currentLocation.coordinate, span: span)
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+        guard let response = response else { return }
+        for mapItem in response.mapItems {
+        self.parks.append(mapItem)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = mapItem.placemark.coordinate
+        annotation.title = mapItem.name
+        self.mapViewA.addAnnotation(annotation)
+        }
+        
+    }
+        
+        
+        
+    }
     
     
 }
